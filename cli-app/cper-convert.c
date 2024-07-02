4482c48b6e2ccf85f03f680a59e0d4ad0ab4e3c5
Andrew Adriance <aadriance@nvidia.com>
2024-07-02 14:55:11 -0700

    Update limits headers for better platform compat
    This change enables building libcper based cli tools on mac systems.
    
    Change-Id: If9b0134d3677b84c57603fa83d546b58e8c2d073
    Signed-off-by: Andrew Adriance <aadriance@nvidia.com>

--- cli-app/cper-convert.c
+++ cli-app/cper-convert.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <libgen.h>
-#include <linux/limits.h>
+#include <limits.h>
 #include <json.h>
 #include "../cper-parse.h"
 #include "../json-schema.h"
--- json-schema.c
+++ json-schema.c
@@ -14,7 +14,6 @@
 #include <json.h>
 #include "json-schema.h"
 #include "edk/BaseTypes.h"
-#include <linux/limits.h>
 
 //Field definitions.
 int json_validator_debug = 0;
